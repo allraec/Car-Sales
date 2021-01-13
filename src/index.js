c8f86846e4874f5f6ff11f4b2b22bb17fb0720e9
Allison Castaneda <allraec@yahoo.com>
2021-01-12 20:23:22 -0600

    final finish reducer structure

--- src/index.js
+++ src/index.js
@@ -3,12 +3,12 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
-import {rootReducer} from "./reducers/index";
+import { reducer } from "./reducers/index";
 
 import 'bulma/css/bulma.css';
 import './styles.scss';
 
-const store = createStore(rootReducer);
+const store = createStore(reducer);
 
 const rootElement = document.getElementById('root');
 ReactDOM.render(
