c7aa80715a4bb185aa48247f46c9d859af0513b3
Allison Castaneda <allraec@yahoo.com>
2021-01-13 19:06:33 -0600

    MVP

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,3 +1,5 @@
+import { ADD_FEATURE, REMOVE_FEATURE} from '../actions/index'
+
 const initialState = {
     additionalPrice: 0,
     car: {
@@ -7,7 +9,7 @@ const initialState = {
           'https://cdn.motor1.com/images/mgl/0AN2V/s1/2019-ford-mustang-bullitt.jpg',
         features: []
     },
-    additionalFeatures: [
+    additionalFeatures: [   
       { id: 1, name: 'V-6 engine', price: 1500 },
       { id: 2, name: 'Racing detail package', price: 1500 },
       { id: 3, name: 'Premium sound system', price: 500 },
@@ -17,10 +19,29 @@ const initialState = {
 
 export const reducer = (state = initialState, action) => {
     switch(action.type) {
-        case ('ADD_FEATURE'):
-            return state;
-        case('REMOVE_FEATURE'):
-            return state;
+        case (ADD_FEATURE):
+            console.log(state);
+            return ({
+                ...state,
+                additionalFeatures: state.additionalFeatures.filter((feature) => feature.id !== action.payload.id),
+                additionalPrice: state.additionalPrice + action.payload.price,
+                car: {
+                    ...state.car,
+                    features: [...state.car.features, action.payload],
+                }
+            });
+        case(REMOVE_FEATURE):
+            console.log(state)
+            return ({
+                ...state,
+                additionalFeatures: [...state.additionalFeatures, action.payload],
+                additionalPrice: state.additionalPrice - action.payload.price,
+                car: {
+                    ...state.car,
+                    features: state.car.features.filter((feature) => feature.id !== action.payload.id)
+                }
+                
+            });
         default:
             return state;
     };
