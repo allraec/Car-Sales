f140999738573ba3de765cef2a08eef9f9eca3a7
Allison Castaneda <allraec@yahoo.com>
2021-03-01 00:30:10 -0600

    redux flow

--- src/App.js
+++ src/App.js
@@ -4,21 +4,33 @@ import Header from './components/Header';
 import AddedFeatures from './components/AddedFeatures';
 import AdditionalFeatures from './components/AdditionalFeatures';
 import Total from './components/Total';
+import connect from 'react-redux';
+import {addFeature, removeFeature} from './actions/index'
 
-const App = () => {
+const App = (props) => {
 
   return (
     <div className="boxes">
       <div className="box">
-        <Header car={state.car} />
-        <AddedFeatures car={state.car} />
+        <Header car={props.car} />
+        <AddedFeatures car={props.car} removeFeature={props.removeFeature}/>
       </div>
       <div className="box">
-        <AdditionalFeatures additionalFeatures={state.additionalFeatures} />
-        <Total car={state.car} additionalPrice={state.additionalPrice} />
+        <AdditionalFeatures additionalFeatures={props.additionalFeatures} addFeature={props.addFeature}/>
+        <Total car={props.car} additionalPrice={props.additionalPrice} />
       </div>
     </div>
   );
 };
 
-export default App;
+const mapStateToProps = (state) => {
+  return{
+    additionalPrice: state.additionalPrice,
+    car: state.car,
+    additionalFeatures: state.additionalFeatures
+  }
+}
+
+const mapDispatchToProps = {addFeature, removeFeature}
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
